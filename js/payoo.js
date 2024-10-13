c51bcc35199ab70c4c80fb4df19a5e59e0364a9c
Ashab Uddin <ashabuddin47@gmail.com>
2024-10-14 00:58:32 +0600

    write some js code for login page

---
+++ js/payoo.js
@@ -0,0 +1,12 @@
+//step-1: set Event handlar
+document.getElementById('btn-login').addEventListener('click',function(event){
+
+    //step-2: prevent default behavior( prevent relodint browser)
+    event.preventDefault();//form sumbit stop reloding the page
+
+    //step-3: get the phone number
+    const phoneNumber = document.getElementById('phone-number');
+
+
+
+})
