66630cccfd02f7bf8508f81a95033fbb7827ac8b
Ashab Uddin <ashabuddin47@gmail.com>
2024-10-14 01:17:01 +0600

    write js code for setup pin and phone number and added home page link

--- js/payoo.js
+++ js/payoo.js
@@ -5,7 +5,18 @@ document.getElementById('btn-login').addEventListener('click',function(event){
     event.preventDefault();//form sumbit stop reloding the page
 
     //step-3: get the phone number
-    const phoneNumber = document.getElementById('phone-number');
+    const phoneNumber = document.getElementById('phone-number').value;
+
+    //step-4: get the pin number
+    const pinNumber = document.getElementById('pin-number').value;
+
+    if(phoneNumber === '01812444115' && pinNumber === '2497'){
+        console.log("Login Successfully");
+        window.location.href = '/home.html';
+    }
+    else{
+        alert("Your phone number or pin is wrong");
+    }
 
 
 
