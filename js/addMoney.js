62c28db60dd8a1d4f4b9c14eb78d06907018d2b3
Ashab Uddin <ashabuddin47@gmail.com>
2024-10-14 22:25:47 +0600

    created a add money function with js code

--- js/addMoney.js
+++ js/addMoney.js
@@ -2,7 +2,7 @@
 s1: event handlers
 prevent page reload after from submit
 s2: get money to the added at account
-s3:  
+s3: verifay the pin number
 */
 //step-1: get event handlers
 document.getElementById('added-money').addEventListener('click',function(event){
@@ -10,9 +10,25 @@ document.getElementById('added-money').addEventListener('click',function(event){
     event.preventDefault();
 
     const amountAdd = document.getElementById('amount-add').value;
-    console.log(amountAdd);
+
     const pinNumber = document.getElementById('pin-number').value;
-    console.log(pinNumber);
+
+    if(pinNumber === '2497'){
+        const accountBanlance = document.getElementById('account-balance').innerText;
+        //convert to the string to number
+        const addMoneyNumber = parseFloat(amountAdd);
+        const accountBanlanceNumber = parseFloat(accountBanlance);
+        const updatedBalance = addMoneyNumber+accountBanlanceNumber;
+        
+        console.log(accountBanlanceNumber)
+        console.log(addMoneyNumber);
+        console.log(updatedBalance);
+        //step-6: update the balance to DOM/UI
+        document.getElementById('account-balance').innerText = updatedBalance;
+    }
 
     
+})
+document.getElementById('log-out').addEventListener('click',function(){
+    window.location.href = '/index.html';
 })
